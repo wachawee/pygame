9c49d205be83001e6e68447ce382ec2e19222053
wachawee <46044664+wachawee@users.noreply.github.com>
2025-04-06 14:45:07 +0700

    Add files via upload

--- star.py
+++ star.py
@@ -1,12 +1,22 @@
 import pygame
 pygame.init()
 screen = pygame.display.set_mode((640,640))
-
+coin_img = pygame.image.load('coin.png').convert()
 running = True
+x = 0
+clock = pygame.time.Clock()
+
+delta_time = 0.1
 while running:
+	screen.blit(coin_img,(x,30))
+	x += 50 * delta_time
+	
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			running = False
-
+	
+	pygame.display.flip()
+	
+	clock.tick(60)
 pygame.quit()
 
